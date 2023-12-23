d056b0cef7118064e5f941e7c10a595ff64a98a9
ahmad juma <juam17020@gmail.com>
2023-12-23 19:22:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (let i = 0; i < names.length; i++) {
+      thankYouMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  module.exports = {
+    writeCards,
+  };
+  
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  module.exports = {
+    writeCards,
+    countDown,
+  };
+  
